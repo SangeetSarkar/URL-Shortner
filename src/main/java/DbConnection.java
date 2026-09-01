215cf7d9775afae1b9a29eb187c4fda1b5a62a76
Sangeet Sarkar <sangeetsarkar2002@gmail.com>
2026-09-02 02:12:45 +0530

    Implement JDBC connection to Postgres EC2

---
+++ src/main/java/DbConnection.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class DbConnection {
+    void connectDB(){
+        final String url = "jdbc:postgresql://65.0.170.126:5432/urlshortner";
+        final Properties props = new Properties();
+        props.setProperty("user", "ubuntu");
+        props.setProperty("password","root");
+
+        try (Connection conn = DriverManager.getConnection(url, props)) {
+            /*System.out.println(conn.getMetaData().getDatabaseProductVersion());*/
+            System.out.println("Connected to DB");
+        } catch(SQLException e) {
+            System.out.println("Error connecting to database " + e.getMessage());
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,20 @@
 public class Main {
     public static void main(String[] args){
-        int strs =12345;
+        int strs = 123;
 
         Encoder en = new Encoder();
-
-        final String encodedString = en.encode(strs);
+        final String encodedString;
+        try {
+            encodedString = en.encode(strs);
+        }catch (IllegalArgumentException e){
+            System.err.println("Entered value is not a valid id");
+            return;
+        }
 
         Decoder de = new Decoder();
 
         final int decodedString = de.decode(encodedString);
+
         System.out.println("Original String: "+strs);
         System.out.println("Encoded-String: "+encodedString);
         System.out.println("Decoded-String: "+decodedString);
